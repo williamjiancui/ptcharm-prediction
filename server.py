aca00598c8d1ca5498e61f5b4e7716af9a541e3a
chen0112 <62029679+chen0112@users.noreply.github.com>
2020-10-24 19:16:19 -0230

    Add files via upload

--- server.py
+++ server.py
@@ -1,26 +1,35 @@
-from flask import Flask, request, jsonify
-import util
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
+import json
 
 app = Flask(__name__)
+model = pickle.load(open('./artifacts/oil_type_prediction_by_MA_Steranes.pickle', 'rb'))
+__data_columns = json.load(open("./artifacts/MA_Steranes_columns.json", 'r'))
 
-@app.route('/predict_oil_type', methods=['POST'])
-def predict_oil_type():
-    x1 = float(request.form['x1'])
-    x2 = float(request.form['x2'])
-    x3 = float(request.form['x3'])
-    x4 = float(request.form['x4'])
-    x5 = float(request.form['x5'])
-    x6 = float(request.form['x6'])
-    x7 = float(request.form['x7'])
 
-    response = jsonify({
-        'estimated_oil_type': util.get_estimated_oil_type(x1, x2, x3, x4, x5, x6, x7)
-    })
-    response.header.add('Access-Control-Allow-Origin', '*')
-    return response
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def get_estimated_oil_type(x1, x2, x3, x4, x5, x6, x7):
+    x = np.zeros(len(__data_columns))
+    x[0] = float(request.form['x1'])
+    x[1] = float(request.form['x2'])
+    x[2] = float(request.form['x3'])
+    x[3] = float(request.form['x4'])
+    x[4] = float(request.form['x5'])
+    x[5] = float(request.form['x6'])
+    x[6] = float(request.form['x7'])
+    x = x.reshape(1, -1)
+    output = __model.predict(x)[0]
+    return render_template('index.html', prediction_test='oil type is $ {}'.format(output))
+
+
 
 
 if __name__ == "__main__":
-    print("Starting Python Flask Server For Oil Type Prediction")
-    util.load_saved_artifacts()
-    app.run()
+    app.run(debug=True)
+
